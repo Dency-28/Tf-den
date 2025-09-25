e83ec82b6118407bac0369859686ac07ff9eb3af
Dency-28 <angelmist2872@gmail.com>
2025-09-25 21:24:59 +0530

    Update statelock.tf

--- statelock.tf
+++ statelock.tf
@@ -43,10 +43,9 @@ resource "aws_dynamodb_table" "tf_locks" {
 
 terraform {
   backend "s3" {
-    bucket         = "tf-state-dency"
-    key            = "global/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-state-locks"
-    encrypt        = true
-  }
+  bucket        = "tf-state-dency"
+  key           = "terraform.tfstate"
+  region        = "us-east-1"
+  use_lockfile  = true
+}
 }
