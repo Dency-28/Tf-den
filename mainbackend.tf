5d0a99dce02c710cc73049c925c5ad564de9f28a
Dency-28 <angelmist2872@gmail.com>
2025-09-30 12:09:16 +0530

    Update mainbackend.tf

--- mainbackend.tf
+++ mainbackend.tf
@@ -1,18 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket        = "tf-state-dency"
-    key           = "terraform.tfstate"
-    region        = "us-east-1"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -20,15 +5,11 @@ provider "aws" {
 resource "aws_s3_bucket" "tf_state" {
   bucket        = "tf-state-dency"
   force_destroy = true
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
+  tags = { Name = "Terraform State Bucket" }
 }
 
 resource "aws_s3_bucket_versioning" "tf_state_versioning" {
   bucket = aws_s3_bucket.tf_state.id
-
   versioning_configuration {
     status = "Enabled"
   }
@@ -36,7 +17,6 @@ resource "aws_s3_bucket_versioning" "tf_state_versioning" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
   bucket = aws_s3_bucket.tf_state.id
-
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -48,13 +28,9 @@ resource "aws_dynamodb_table" "tf_locks" {
   name         = "tf-state-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-
   attribute {
     name = "LockID"
     type = "S"
   }
-
-  tags = {
-    Name = "Terraform State Lock Table"
-  }
+  tags = { Name = "Terraform State Lock Table" }
 }
