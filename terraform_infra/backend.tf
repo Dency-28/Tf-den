70fe4a589e860aaf14b5a7c09a192bcc0794225c
Dency-28 <angelmist2872@gmail.com>
2025-11-03 16:52:12 +0530

    Add DynamoDB table and encryption to S3 backend

--- terraform_infra/backend.tf
+++ terraform_infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tf-state-dency"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "tf-state-dency"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-state-locks"
+    encrypt        = true
   }
 }
-
-
